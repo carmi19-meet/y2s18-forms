51c6b1d671b89172ccd0a1a38839a952714f2bff
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 10:26:34 +0300

    forms

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +10,13 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
-app.run(debug=True)
+@app.route('/add',methods=['GET', 'POST'])
+def add_student_route():
+    if request.method == 'GET':
+        return render_template("add.html")
+    else:
+        print("Received POST request!")
+        return(render_template("add.html"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- lab/app.py
+++ lab/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,5 +18,13 @@ def add_student_route():
 		add_student(request.form['student_name'],
 			request.form['student_year'], False)
 		return render_template('add.html')
+
+@app.route('/delete/<int:student_id>', methods=['POST'])
+def delete(student_id):
+	a = query_by_id(student_id)
+	delete_student(a.name)
+	return redirect(url_for('/'))
+
+
 
 app.run(debug=True)
